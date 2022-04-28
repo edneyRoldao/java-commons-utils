556a0f41173d6ac8332777e6f8c13fe0a8977132
edneyRoldao <edneyroldao@gmail.com>
2022-04-28 00:32:26 -0300

    first commit with base project

---
+++ src/main/java/br/com/edn/commonsutils/CommonsUtilsApplication.java
@@ -0,0 +1,13 @@
+package br.com.edn.commonsutils;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommonsUtilsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommonsUtilsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/edn/commonsutils/CommonsUtilsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.edn.commonsutils;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommonsUtilsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
